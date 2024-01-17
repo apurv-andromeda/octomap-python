c0fe4bd34f34914205394bb9042b1dd6157c0d0b
Apurv Tiwari <apurv@andromedasurgical.com>
2024-01-16 21:32:01 -0800

    pyproject contains build dependencies and setup.py updated

--- setup.py
+++ setup.py
@@ -2,15 +2,17 @@ import subprocess
 import sys
 
 from setuptools import Extension
+from Cython.Distutils import build_ext
+import numpy
+from skbuild import setup
 
 
 # https://github.com/skvark/opencv-python/blob/master/setup.py
-def install_packages(*requirements):
+def install_packages(name):
     # No more convenient way until PEP 518 is implemented;
     # setuptools only handles eggs
-    subprocess.check_call(
-        [sys.executable, "-m", "pip", "install"] + list(requirements)
-    )
+    print(sys.path)
+    subprocess.run([sys.executable, "-m", "pip", "install" + name], shell=True)
 
 
 # https://github.com/skvark/opencv-python/blob/master/setup.py
@@ -18,20 +20,18 @@ def get_or_install(name, version=None):
     # Do not import 3rd-party modules into the current process
     import json
 
-    js_packages = json.loads(
-        subprocess.check_output(
-            [sys.executable, "-m", "pip", "list", "--format", "json"]
-        ).decode("ascii")
-    )  # valid names & versions are ASCII as per PEP 440
-    try:
-        [package] = (
-            package for package in js_packages if package["name"] == name
-        )
-    except ValueError:
-        install_packages("%s==%s" % (name, version) if version else name)
-        return version
-    else:
-        return package["version"]
+    # js_packages = json.loads(
+    #     subprocess.run(["pip", "list", "--format", "json"]).decode("ascii")
+    # )  # valid names & versions are ASCII as per PEP 440
+    # try:
+    #     [package] = (
+    #         package for package in js_packages if package["name"] == name
+    #     )
+    # except ValueError:
+    install_packages(name)
+    #     return version
+    # else:
+    #     return package["version"]
 
 
 def get_long_description():
@@ -49,14 +49,6 @@ def get_long_description():
 
 
 def main():
-    get_or_install("cython")
-    get_or_install("numpy")
-    get_or_install("scikit-build")
-
-    from Cython.Distutils import build_ext
-    import numpy
-    import skbuild
-
     ext_modules = [
         Extension(
             "octomap",
@@ -78,7 +70,7 @@ def main():
         )
     ]
 
-    skbuild.setup(
+    setup(
         name="octomap-python",
         version="1.8.0.post12",
         install_requires=["numpy"],
